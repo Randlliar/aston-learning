ae8d7fb5fde28fd0339f047015690082d73b6071
Igor Sharkevich <sharkevich.i@inbox.ru>
2025-02-28 22:04:47 +0300

    feat: add myPromt function

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-function helloWorld() {
-    console.log("Hello, World!");
+function myPrompt() {
+    let firstNumInput = prompt('Ввведите число');
+    let secondNumInput = prompt('Введите систему счисления для числа');
+
+    let firstNum = parseInt(firstNumInput, 10);
+    let secondNum = parseInt(secondNumInput, 10);
+
+    if (isNaN(firstNum) || Number.isInteger(firstNum)) {
+        alert("Некорректный ввод!");
+        return;
+    }
+
+    if (isNaN(secondNum) || Number.isInteger(secondNum) || secondNum < 2 || secondNum > 36) {
+        alert("Некорректный ввод!");
+        return;
+    }
+
+    let convertedNum = firstNum.toString(secondNum);
+    alert(`Ответ: число ${firstNum} в ${secondNum}-ой системе счисления = ${convertedNum}`);
 }
 
-helloWorld();
-
-function helloGit() {
-    console.log("Hello, Git!");
-}
-
-helloGit();
-
-function sum(a, b) {
-    a = 2;
-    b = 3;
-    console.log(a + b);
-}
-
-sum();
+myPrompt();
